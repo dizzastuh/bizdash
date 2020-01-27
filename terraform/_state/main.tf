0584d2a576fd0b07358bf3051b9958f56b6f38cc
blake-deadly <dizzastuh@gmail.com>
2020-01-26 21:32:31 -0600

    more tf pieces

--- terraform/_state/main.tf
+++ terraform/_state/main.tf
@@ -16,6 +16,18 @@ resource "google_storage_bucket" "tf-state-bucket" {
   storage_class = "REGIONAL"
 
   versioning = {
-      enabled = true
+    enabled = true
   }
+}
+
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/storage.admin"
+    members = var.bucket_users
+  }
+}
+
+resource "google_storage_bucket_iam_policy" "editor" {
+  bucket = "${google_storage_bucket.default.name}"
+  policy_data = "${data.google_iam_policy.admin.policy_data}"
 }
--- terraform/_state/variables.tf
+++ terraform/_state/variables.tf
@@ -8,3 +8,8 @@ variable "location" {
     description = "Location code (ex: US, EU)"
     type = "string"
 }
+
+variable "bucket_users" {
+    description = "List of users that can access the bucket"
+    type = list(string)
+}
