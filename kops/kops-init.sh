817f35f7bdbb14cfab41d3420d51c3dd3751ecf3
blake-deadly <dizzastuh@gmail.com>
2020-01-27 23:51:46 -0600

    wrapper for kops cluster setup

---
+++ kops/kops-init.sh
@@ -0,0 +1,9 @@
+PROJECT=`gcloud config get-value project`
+
+if [ -z "$PROJECT" ]; then
+  echo "Could not find a project.  Is your config enabled?"
+  exit 1
+fi
+
+kops create cluster ${CLUSTER} --cloud ${CLOUD} --zones {ZONES} --state ${KOPS_STATE_STORE} --project=${PROJECT}
+kops update cluster --name ${CLUSTER} --yes
