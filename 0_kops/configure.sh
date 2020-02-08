7c51b380f1cd6be8a0b513c87b3c14df002c8fea
blake-deadly <dizzastuh@gmail.com>
2020-02-08 17:19:37 -0600

    more kops things

---
+++ 0_kops/configure.sh
@@ -0,0 +1,3 @@
+gcloud auth application-default login
+gcloud config set project $PROJECT
+gcloud config set computezone $ZONES
---
+++ 0_kops/create_cluster.sh
@@ -0,0 +1,17 @@
+echo "CREATING CLUSTER..."
+kops create cluster \
+       --cloud gce \
+       --state $KOPS_STATE_STORE \
+       --zones $ZONES \
+       --name $CLUSTER \
+       --project $PROJECT \
+       --yes
+
+echo "UPDATING CLUSTER..."
+kops update cluster --name $CLUSTER --yes
+
+echo "UPDATING CLUSTER..."
+kops rolling-update cluster $CLUSTER
+
+kops export kubecfg $CLUSTER
+
