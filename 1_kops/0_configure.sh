24dbab0d46d3fdb400082a26d73181e59d311932
blake-deadly <dizzastuh@gmail.com>
2020-02-09 10:06:06 -0600

    perusing samples

--- 1_kops/0_configure.sh
+++ 1_kops/0_configure.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 gcloud auth application-default login
 gcloud config set project $PROJECT
 gcloud config set computezone $ZONES
--- 1_kops/update_kops_version.sh
+++ 1_kops/update_kops_version.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo rm -rf /usr/local/bin/kops
 wget -O kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x ./kops
