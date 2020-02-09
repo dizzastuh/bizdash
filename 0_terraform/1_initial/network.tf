a05e8e351e68e507c84643feb1f7366cd2d29410
blake-deadly <dizzastuh@gmail.com>
2020-02-08 19:35:28 -0600

    nope this isn't helping

--- 0_terraform/1_initial/network.tf
+++ 0_terraform/1_initial/network.tf
@@ -3,16 +3,16 @@
 #   name = "${local.name_prefix}-ip"
 # }
 
-resource "google_dns_managed_zone" "zone" {
-  name        = var.project
-  dns_name    = "${var.project}.${var.domain_name}."
-  description = "DNS Zone for ${title(var.project)} App"
+# resource "google_dns_managed_zone" "zone" {
+#   name        = var.project
+#   dns_name    = "${var.project}.${var.domain_name}."
+#   description = "DNS Zone for ${title(var.project)} App"
   
-  labels = {
-    env  = var.env
-    proj = var.project
-  }
-}
+#   labels = {
+#     env  = var.env
+#     proj = var.project
+#   }
+# }
 
 # resource "google_dns_record_set" "cluster" {
 #   name    = "${var.project}.${google_dns_managed_zone.zone.dns_name}"
